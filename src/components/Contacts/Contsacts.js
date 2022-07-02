336ece74c3e800527559cea69ba790633b46db7c
Bobshop verbytski <nikolasdelunko@gmail.com>
2022-07-02 17:40:24 +0300

    Contacts 90%

--- src/components/Contacts/Contsacts.js
+++ src/components/Contacts/Contsacts.js
@@ -1,13 +1,38 @@
 import React from 'react'
-import {Box, Typography} from '@mui/material'
+import {Box, Typography, Button, Stack, Grid} from '@mui/material'
 import {useStyles} from "./Style"
+import useColors from '../../utils/costumHooks/useColors'
 
 
 export default function Contsacts() {
+    const { colorsHendler } = useColors()
     const classes = useStyles() 
   return (
     <Box className={classes.father}>
-        <Typography variant="h4" component="h4" color="#C8CAD6" className={classes.textHead}>C0ntacts</Typography>
+        <Box className={classes.textBlock}>
+        <Typography variant="h4" component="h4" color="#C8CAD6" className={classes.textHead}>Contacts</Typography>
+        <Typography variant="h5" component="h4" color="#C8CAD6" className={classes.text}>verbytski@ukr.net</Typography>
+        <Typography variant="h5" component="h2" color="#C8CAD6" className={classes.text}>(+380) 974 97 128</Typography>
+        </Box>
+        <Box>
+        <Stack spacing={2} direction="row" className={classes.btnBox}>
+          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid item md={3} sm={12}>
+           <Button variant="text" className={classes.btn} sx={{ borderTop: `2px solid ${colorsHendler(10)}`}}>linkedin</Button>
+            </Grid>
+            <Grid item md={3} sm={12}>
+                <Button variant="text" className={classes.btn} sx={{ borderTop: `2px solid ${colorsHendler(10)}`}}>githab</Button>
+            </Grid>
+            <Grid item md={3} sm={12}>
+            <Button variant="text" className={classes.btn} sx={{ borderTop: `2px solid ${colorsHendler(10)}`}}>telegram</Button>
+            </Grid>
+            <Grid item md={3} sm={12}>
+                
+             <Button variant="text" className={classes.btn} sx={{ borderTop: `2px solid ${colorsHendler(10)}`}}>download cv</Button>
+            </Grid>
+             </Grid>
+         </Stack>
+        </Box>
     </Box>
   )
 }
--- src/components/Contacts/Style.js
+++ src/components/Contacts/Style.js
@@ -2,15 +2,46 @@ import { makeStyles } from '@mui/styles'
 
 
 export const useStyles = makeStyles(theme => ({
-    father: { margin: '0 0 0 10%',
+    father: { 
+        margin: '0 0 0 10%',
+        display: 'flex',
+        flexDirection: 'column',
+    
 },
     textHead: {  
-        paddingTop: '100px',
+        fontWeight: 'bolder !important',
+        lineHeight: '110%',
+        paddingTop: '15px',
         display: 'flex',
         fontStyle: 'normal',
-        fontWeight: '700',
-        fontSize: '48px',
+        // fontSize: '48px',
+        paddingBottom: '50px',
         textTransform: 'uppercase',
     },
-    
+    text: {  
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize:' 48px',
+        paddingTop: '15px',
+        display: 'flex',
+        textTransform: 'uppercase',
+    },
+    textBlock: {
+        padding: '150px 0 102px 0',
+    },
+    btn: {
+        padding: '15px 65px !important',
+        "&:hover": {
+            backgroundColor : "#FF2055 !important",
+        }
+
+    },
+    btnBox: {
+        marginLeft: 'auto',
+        textAlign: 'left',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '63px',
+        paddingBottom: '225px',
+    }
 }))
