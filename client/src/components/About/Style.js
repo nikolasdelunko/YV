83f120a5b656807f66c2e2b2abd665a9b76bc3ac
Bobshop verbytski <nikolasdelunko@gmail.com>
2022-09-22 20:46:49 +0300

    add mob about

--- client/src/components/About/Style.js
+++ client/src/components/About/Style.js
@@ -1,126 +1,139 @@
-import { makeStyles } from '@mui/styles'
+import { makeStyles } from "@mui/styles";
 
-export const useStyles = makeStyles(theme => ({
-    textHead: {  
-        display: 'flex',
-        fontStyle: 'normal',
-        fontSize: '48px',
-        textTransform: 'uppercase',
-        padding: '100px 0px 0px 10%',
-        fontWeight: 'bolder !important',
-        lineHeight: '110%',
-        paddingTop: '15px',
-        // fontSize: '48px',
-        paddingBottom: '50px',
+export const useStyles = makeStyles((theme) => ({
+  textHead: {
+		fontFmily: "Gilroy !important",
+    display: "flex",
+    fontStyle: "normal",
+    fontSize: "48px",
+    textTransform: "uppercase",
+    fontWeight: "700 !important",
+    padding: "100px 0px 0px 10%",
+    lineHeight: "110%",
+    paddingTop: "15px",
+    paddingBottom: "50px",
+		textAlign: 'left',
+  },
+  box: {
+    width: "50%",
+    padding: "10px ",
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    ["@media (max-width:780px)"]: {
+      width: "100%",
     },
-    box: {
-        width:'50%',
-        padding: '10px ',
-        display: 'flex',
-        justifyContent: 'center',
-        flexWrap: 'wrap',
+  },
+  link: {
+    textDecoration: "none",
+    textAlign: "center",
+  },
+  boxCard: {
+    paddingTop: "200px",
+  },
+  conteinerCard: {
+    paddingLeft: "10%",
+    marginBottom: "30px",
+		["@media (max-width:780px)"]: {
+			width: "90%",
     },
-    link: {
-        textDecoration: 'none',
-        textAlign: 'center',
+  },
+  text: {
+    padding: "15px 5px 0 5px",
+		fontFamily: 'Gilroy',
+    fontStyle: "normal",
+    fontWeight: "500 !important",
+    fontSize: " 48px",
+    display: "flex",
+  },
+  textBox: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+  },
+  textBlock: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  mainCard: {
+    width: "460px",
+    height: "238px",
+    left: "100px",
+    top: "850px",
+    background: "#1D1D1D",
+    boxShadow: "0px 3px 3px 3px rg",
+		["@media (max-width:780px)"]: {
+			width: "97%",
     },
-    boxCard: {
-    paddingTop: '200px',
+  },
+  mainCardExp: {
+    maxWidth: "685px",
+    background: "#1D1D1D",
+    boxShadow: " 0px 3px 3px 3px rgba(0, 0, 0, 0.15)",
+    ["@media (max-height:820px)"]: {
+      maxWidth: "485px",
     },
-    conteinerCard: {
-        paddingLeft: '10%',
-        marginBottom: '30px'
+		["@media (max-width:780px)"]: {
+			width: "97%",
     },
-    text: {  
-        padding: '15px 5px 0 5px',
-        fontStyle: 'normal',
-        fontWeight: '700',
-        fontSize:' 48px',
-        display: 'flex',
-    },
-    textBox: {
-        display: 'flex',
-        justifyContent: 'center',
-        flexWrap: 'wrap',
-    },
-    textBlock: {
-        display: 'flex',
-        justifyContent: 'center',
-    },
-    mainCard: { 
-        width: '460px',
-        height: '238px',
-        left: '100px',
-        top: '850px',
-        background: '#1D1D1D',
-        boxShadow: '0px 3px 3px 3px rg',
-    
-},
-    mainCardExp: {
-        maxWidth: '685px',   
-        background: '#1D1D1D',      
-        boxShadow:' 0px 3px 3px 3px rgba(0, 0, 0, 0.15)',
-        ["@media (max-height:820px)"] : {
-            maxWidth: '485px',
-          },
-    },
-    cardTextUp: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '28px 10% 0',
-},
-    cardTextDuwn: {
-        margin: '26px 0 0 10%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-},
-    cardTextDuwnExp: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        margin: '8px 10% 32px',
-},
-    cardItems: {
-        display: "flex",
-        flexWrap: "wrap"
-    },
-    textH: {
-        fontStyle: 'normal',
-        letterSpacing: '0.03em',
-        textTransform: 'uppercase',
-        color: "#C8CAD6",
-        fontWeight: '500 !important',
-    },
-    textLink: {
-        color: '#AFA9A9',
-        textDecorationLine: 'underline',
-        fontSize: '16px',
-    },
-    textSpec: {
-        color: '#787878',
-        fontSize: '18px !important',
-        fontWeight: '500 !important',
-    },
-    textYear: {
-        color: '#FFFFFF',
-        fontWeight: "400 !important",
-        fontSize: '20px !important',
-    },
-    textDescriptBox:{
-        margin: '0 10%'
-    },
-    textDescript: {
-        textAlign: 'left',
-        maxWidth: "585px",
-        color: '#CCCCD5',
-        fontWeight: "500 !important",
-        fontSize: '18px !important',
-        paddingBottom: '45px',
-    },
-    photo: {
-        width: "70%",
-        height: '30%',
-        objectFit: 'cover',
-    },
-}))
+  },
+  cardTextUp: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "28px 10% 0",
+  },
+  cardTextDuwn: {
+    margin: "26px 0 0 10%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+  },
+  cardTextDuwnExp: {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "8px 10% 32px",
+  },
+  cardItems: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  textH: {
+    fontStyle: "normal",
+    letterSpacing: "0.03em",
+    textTransform: "uppercase",
+    color: "#C8CAD6",
+    fontWeight: "500 !important",
+  },
+  textLink: {
+    color: "#AFA9A9",
+    textDecorationLine: "underline",
+    fontSize: "16px",
+  },
+  textSpec: {
+    color: "#787878",
+    fontSize: "18px !important",
+    fontWeight: "500 !important",
+  },
+  textYear: {
+    color: "#FFFFFF",
+    fontWeight: "400 !important",
+    fontSize: "20px !important",
+  },
+  textDescriptBox: {
+    margin: "0 10%",
+  },
+  textDescript: {
+    textAlign: "left",
+    maxWidth: "585px",
+    color: "#CCCCD5",
+    fontWeight: "500 !important",
+    fontSize: "18px !important",
+    paddingBottom: "45px",
+  },
+  photo: {
+    width: "70%",
+    height: "30%",
+    objectFit: "cover",
+  },
+}));
--- client/src/components/Contacts/Contsacts.js
+++ client/src/components/Contacts/Contsacts.js
@@ -1,66 +1,126 @@
-import React from 'react'
-import {Box, Typography, Button, Stack, Grid} from '@mui/material'
-import {useStyles} from "./Style"
-import useColors from '../../utils/costumHooks/useColors'
-import { useEffect, useState } from 'react';
-import axios from 'axios'
-import {getCv } from "../../utils/api/cvApi"
-
+import React from "react";
+import { Box, Typography, Button, Stack, Grid } from "@mui/material";
+import { useStyles } from "./Style";
+import useColors from "../../utils/costumHooks/useColors";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { getCv } from "../../utils/api/cvApi";
 
 export default function Contsacts() {
-    const { colorsHendler } = useColors()
-    const classes = useStyles() 
-    const [data, setData] = useState()
-		const [cv, setCv] = useState()
+  const { colorsHendler } = useColors();
+  const classes = useStyles();
+  const [data, setData] = useState();
+  const [cv, setCv] = useState();
 
-    const getInfo = async() => {
-      const res =  await axios.get('http://localhost:3009/contacts')
-      return setData(res.data[0])
-  }
+  const getInfo = async () => {
+    const res = await axios.get("http://localhost:3009/contacts");
+    return setData(res.data[0]);
+  };
 
-	const getCV = async() => {
-		const res = await getCv()
-		return setCv(res.data[0])
-}
+  const getCV = async () => {
+    const res = await getCv();
+    return setCv(res.data[0]);
+  };
 
-      useEffect(() => {
-       getInfo()
-			 getCV()
-      }, [])
+  useEffect(() => {
+    getInfo();
+    getCV();
+  }, []);
 
   return (
     <Box className={classes.father}>
-        <Box className={classes.textBlock} id="contact">
-        <Typography variant="h4" component="h4" color="#C8CAD6" className={classes.textHead}>Contacts</Typography>
-        <Typography variant="h5" component="h4" color="#C8CAD6" className={classes.text}>{data?.email}</Typography>
-        <Typography variant="h5" component="h2" color="#C8CAD6" className={classes.text}>{data?.phone}</Typography>
-        </Box>
-        <Box>
+      <Box className={classes.textBlock} id="contact">
+        <Typography
+          variant="h4"
+          component="h4"
+          color="#C8CAD6"
+          className={classes.textHead}
+        >
+          Contacts
+        </Typography>
+        <Typography
+          variant="h5"
+          component="h4"
+          color="#C8CAD6"
+          className={classes.text}
+        >
+          {data?.email}
+        </Typography>
+        <Typography
+          variant="h5"
+          component="h2"
+          color="#C8CAD6"
+          className={classes.text}
+        >
+          {data?.phone}
+        </Typography>
+      </Box>
+      <Box>
         <Stack spacing={2} direction="row" className={classes.btnBox}>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
             <Grid item md={3} sm={12}>
-            <a href={'https://www.linkedin.com/in/yaroslav-verbytski/'} className={classes.link}>
-           <Button variant="text" className={classes.btn} sx={{ borderTop: `2px solid ${colorsHendler(10)}`}}>linkedin</Button>
-           </a>
+              <a
+                href={"https://www.linkedin.com/in/yaroslav-verbytski/"}
+                target="blanc"
+                className={classes.link}
+              >
+                <Button
+                  variant="text"
+                  className={classes.btn}
+                  sx={{ borderTop: `2px solid ${colorsHendler(10)}` }}
+                >
+                  linkedin
+                </Button>
+              </a>
             </Grid>
             <Grid item md={3} sm={12}>
-             <a href={'https://github.com/nikolasdelunko'} className={classes.link}>
-                 <Button variant="text" className={classes.btn} sx={{ borderTop: `2px solid ${colorsHendler(10)}`}}>githab</Button>
-             </a>
+              <a
+                href={"https://github.com/nikolasdelunko"}
+                target="blanc"
+                className={classes.link}
+              >
+                <Button
+                  variant="text"
+                  className={classes.btn}
+                  sx={{ borderTop: `2px solid ${colorsHendler(10)}` }}
+                >
+                  githab
+                </Button>
+              </a>
             </Grid>
             <Grid item md={3} sm={12}>
-            <a href={'https://t.me/verbytskiYar'} className={classes.link}>
-            <Button variant="text" className={classes.btn} sx={{ borderTop: `2px solid ${colorsHendler(10)}`}}>telegram</Button>
-            </a>
+              <a
+                href={"https://t.me/verbytskiYar"}
+                target="blanc"
+                className={classes.link}
+              >
+                <Button
+                  variant="text"
+                  className={classes.btn}
+                  sx={{ borderTop: `2px solid ${colorsHendler(10)}` }}
+                >
+                  telegram
+                </Button>
+              </a>
             </Grid>
-            <Grid item md={3} sm={12}>   
-						<a href={cv?.link} target='blanc' className={classes.link}> 
-             <Button variant="text" className={classes.btn} sx={{ borderTop: `2px solid ${colorsHendler(10)}`}}>download cv</Button>
-             </a>
+            <Grid item md={3} sm={12}>
+              <a href={cv?.link} target="blanc" className={classes.link}>
+                <Button
+                  variant="text"
+                  className={classes.btn}
+                  sx={{ borderTop: `2px solid ${colorsHendler(10)}` }}
+                >
+                  download cv
+                </Button>
+              </a>
             </Grid>
-             </Grid>
-         </Stack>
-        </Box>
+          </Grid>
+        </Stack>
+      </Box>
     </Box>
-  )
+  );
 }
